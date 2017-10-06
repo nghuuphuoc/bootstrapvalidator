686c1484984d16590604230188666f86b8d418b8
abduwaly <arsay@foxmail.com>
2017-10-06 19:11:32 +0800

    ug-CN.js
    added new language : Uyghur   >>>  https://en.wikipedia.org/wiki/Uyghurs

---
+++ src/js/language/ug-CN.js
@@ -0,0 +1,370 @@
+(function ($) {
+    /**
+     * Uyghur language package
+     * Translated by @ Abduwaly
+     */
+    $.fn.bootstrapValidator.i18n = $.extend(true, $.fn.bootstrapValidator.i18n, {
+        base64: {
+            'default': 'كىرگۈزۈڭBase64'
+        },
+        between: {
+            'default': ' نىڭ ئارىسىدىكى سىان كىرگۈزۈڭ %s بىلەن %s ',
+            notInclusive: 'نىڭ ئارىسىدىكى سىان كىرگۈزۈڭ %s بىلەن %s'
+        },
+        callback: {
+            'default': 'ئۈنۈملۈك سان كىرگۈزۈڭ'
+        },
+        choice: {
+            'default': 'ئۈنۈملۈك سان كىرگۈزۈڭ',
+            less: 'نى تاللاڭ %s ئاز دىگەندە ',
+            more: 'دىن ئوشۇق تاللاشقا بولمايدۇ %s',
+            between: 'گىچە تاللاڭ %s دىن %s'
+        },
+        color: {
+            'default': 'ئۈنۈملۈك رەڭ كىرگۈزۈڭ'
+        },
+        creditCard: {
+            'default': 'ئۈنۈملۈك ئىناۋەت كارتىسى كىرگۈزۈڭ'
+        },
+        cusip: {
+            'default': 'كودى كىرگۈزۈڭ CUSIP ئۈنۈملۈك'
+        },
+        cvv: {
+            'default': 'كودى كىرگۈزۈڭ CVV ئۈنۈملۈك'
+        },
+        date: {
+            'default': 'ئۈنۈملۈك چىسىلا كىرگۈزۈڭ', 
+            min: 'دىن كىيىنكى چىسىلانى كىرگۈزۈڭ %s',
+            max: 'دىن بۇرۇنقى چىسىلانى كىرگۈزۈڭ %s',
+            range: ' نىڭ ئارىسىدىكى چىسىلانى كىرگۈزۈڭ %s بىلەن %s'
+        },
+        different: {
+            'default': 'ئوخشىمىغان سانلارنى كىرگۈزۈڭ'
+        },
+        digits: {
+            'default': 'ئۈنۈملۈك سان كىرگۈزۈڭ'
+        },
+        ean: {
+            'default': 'كودى كىرگۈزۈڭ EAN ئۈنۈملۈك'
+        },
+        emailAddress: {
+            'default': 'ئۈنۈملۈك ئېلخەت ئادىرىسى كىرگۈزۈڭ'
+        },
+        file: {
+            'default': 'ئۈنۈملۈك ھۆججەتنى تاللاڭ'
+        },
+        greaterThan: {
+            'default': ' دىن چوڭ ساننى كىرگۈزۈڭ %s',
+            notInclusive: ' دىن چوڭ ساننى كىرگۈزۈڭ %s'
+        },
+        grid: {
+            'default': 'كودى كىرگۈزۈڭ GRId ئۈنۈملۈك'
+        },
+        hex: {
+            'default': '16لىك سېستىما دىكى ساننى كىرگۈزۈڭ'
+        },
+        hexColor: {
+            'default': '16لىك سېستىما دىكى رەڭ نۇمۇرىنى كىرگۈزۈڭ'
+        },
+        iban: {
+            'default': 'كودى كىرگۈزۈڭ IBAN ئۈنۈملۈك',
+            countryNotSupported: '%s  قوللىمايدىغان دۆلەت ۋە رايون',
+            country: ' نۇمۇرىنى كىرگۈزۈڭ IBAN نىڭ ئۈنۈملۈك %s',
+            countries: {
+                AD: 'Andorra',
+                AE: 'United Arab Emirates',
+                AL: 'Albania',
+                AO: 'Angola',
+                AT: 'Austria',
+                AZ: 'Azerbaijan',
+                BA: 'Bosnia and Herzegovina',
+                BE: 'Belgium',
+                BF: 'Burkina Faso',
+                BG: 'Bulgaria',
+                BH: 'Bahrain',
+                BI: 'Burundi',
+                BJ: 'Benin',
+                BR: 'Brazil',
+                CH: 'Switzerland',
+                CI: 'Ivory Coast',
+                CM: 'Cameroon',
+                CR: 'Costa Rica',
+                CV: 'Cape Verde',
+                CY: 'Cyprus',
+                CZ: 'Czech Republic',
+                DE: 'Germany',
+                DK: 'Denmark',
+                DO: 'Dominican Republic',
+                DZ: 'Algeria',
+                EE: 'Estonia',
+                ES: 'Spain',
+                FI: 'Finland',
+                FO: 'Faroe Islands',
+                FR: 'France',
+                GB: 'United Kingdom',
+                GE: 'Georgia',
+                GI: 'Gibraltar',
+                GL: 'Greenland',
+                GR: 'Greece',
+                GT: 'Guatemala',
+                HR: 'Croatia',
+                HU: 'Hungary',
+                IE: 'Ireland',
+                IL: 'Israel',
+                IR: 'Iran',
+                IS: 'Iceland',
+                IT: 'Italy',
+                JO: 'Jordan',
+                KW: 'Kuwait',
+                KZ: 'Kazakhstan',
+                LB: 'Lebanon',
+                LI: 'Liechtenstein',
+                LT: 'Lithuania',
+                LU: 'Luxembourg',
+                LV: 'Latvia',
+                MC: 'Monaco',
+                MD: 'Moldova',
+                ME: 'Montenegro',
+                MG: 'Madagascar',
+                MK: 'Macedonia',
+                ML: 'Mali',
+                MR: 'Mauritania',
+                MT: 'Malta',
+                MU: 'Mauritius',
+                MZ: 'Mozambique',
+                NL: 'Netherlands',
+                NO: 'Norway',
+                PK: 'Pakistan',
+                PL: 'Poland',
+                PS: 'Palestine',
+                PT: 'Portugal',
+                QA: 'Qatar',
+                RO: 'Romania',
+                RS: 'Serbia',
+                SA: 'Saudi Arabia',
+                SE: 'Sweden',
+                SI: 'Slovenia',
+                SK: 'Slovakia',
+                SM: 'San Marino',
+                SN: 'Senegal',
+                TN: 'Tunisia',
+                TR: 'Turkey',
+                VG: 'Virgin Islands, British'
+            }
+        },
+        id: {
+            'default': 'ئۈنۈملۈك كىملىك نۇمۇرىنى كىرگۈزۈڭ',
+            countryNotSupported: '%s  قوللىمايدىغان دۆلەت ۋە رايون',
+            country: 'نىڭ ئۈنۈملۈك كىملىك نۇمۇرىنى كىرگۈزۈڭ %s',
+            countries: {
+                BA: 'Bosnia and Herzegovina',
+                BG: 'Bulgaria',
+                BR: 'Brazil',
+                CH: 'Switzerland',
+                CL: 'Chile',
+                CN: 'China',
+                CZ: 'Czech Republic',
+                DK: 'Denmark',
+                EE: 'Estonia',
+                ES: 'Spain',
+                FI: 'Finland',
+                HR: 'Croatia',
+                IE: 'Ireland',
+                IS: 'Iceland',
+                LT: 'Lithuania',
+                LV: 'Latvia',
+                ME: 'Montenegro',
+                MK: 'Macedonia',
+                NL: 'Netherlands',
+                RO: 'Romania',
+                RS: 'Serbia',
+                SE: 'Sweden',
+                SI: 'Slovenia',
+                SK: 'Slovakia',
+                SM: 'San Marino',
+                TH: 'Thailand',
+                ZA: 'South Africa'
+            }
+        },
+        identical: {
+            'default': 'ئوخشاش سانلارنى كىرگۈزۈڭ'
+        },
+        imei: {
+            'default': 'نى كىرگۈزۈڭ IMEI ئۈنۈملۈك'
+        },
+        imo: {
+            'default': 'نۇمۇرىنى كىرگۈزۈڭ IMO ئۈنۈملۈك '
+        },
+        integer: {
+            'default': 'ئۈنۈملۈك پۈتۈن سان كىرگۈزۈڭ'
+        },
+        ip: {
+            'default': ' ئادىرىسى كىرگۈزۈڭ IP ئۈنۈملۈك',
+            ipv4: ' ئادىرىسى كىرگۈزۈڭ IPv4 ئۈنۈملۈك',
+            ipv6: ' ئادىرىسى كىرگۈزۈڭ IPv6 ئۈنۈملۈك'
+        },
+        isbn: {
+            'default': 'نۇمۇرىنى كىرگۈزۈڭ ISBN ئۈنۈملۈك '
+        },
+        isin: {
+            'default': 'نۇمۇرىنى كىرگۈزۈڭ ISIN ئۈنۈملۈك '
+        },
+        ismn: {
+            'default': 'نۇمۇرىنى كىرگۈزۈڭ ISMN ئۈنۈملۈك '
+        },
+        issn: {
+            'default': 'نۇمۇرىنى كىرگۈزۈڭ ISSN ئۈنۈملۈك '
+        },
+        lessThan: {
+            'default': ' دىن كىچىك ساننى كىرگۈزۈڭ %s',
+            notInclusive: 'دىن كىچىك ساننى كىرگۈزۈڭ %s'
+        },
+        mac: {
+            'default': 'ئادىرىسى كىرگۈزۈڭ MAC ئۈنۈملۈك'
+        },
+        meid: {
+            'default': 'نۇمۇرىنى كىرگۈزۈڭ MEID ئۈنۈملۈك '
+        },
+        notEmpty: {
+            'default': 'چوقۇم توشقۇزۇش كىرەك'
+        },
+        numeric: {
+            'default': 'ئۈنۈملۈك سان كىرگۈزۈڭ'
+        },
+        phone: {
+            'default': 'ئۈنۈملۈك تېلفۇن نۇمۇرىنى كىرگۈزۈڭ',
+            countryNotSupported: '%s  قوللىمايدىغان دۆلەت ۋە رايون',
+            country: ' نىڭ ئۈنۈملۈك تېلفۇن نۇمۇرىنى كىرگۈزۈڭ %s',
+            countries: {
+                BR: 'Brazil',
+                CN: 'China',
+                CZ: 'Czech Republic',
+                DE: 'Germany',
+                DK: 'Denmark',
+                ES: 'Spain',
+                FR: 'France',
+                GB: 'United Kingdom',
+                MA: 'Morocco',
+                PK: 'Pakistan',
+                RO: 'Romania',
+                RU: 'Russia',
+                SK: 'Slovakia',
+                TH: 'Thailand',
+                US: 'USA',
+                VE: 'Venezuela'
+            }
+        },
+        regexp: {
+            'default': 'قائىدىگە چۈشىدىغان قىممەت كىرگۈزۈڭ'
+        },
+        remote: {
+            'default': 'ئۈنۈملۈك قىممەت كىرگۈزۈڭ'
+        },
+        rtn: {
+            'default': 'نۇمۇرىنى كىرگۈزۈڭ RTN ئۈنۈملۈك'
+        },
+        sedol: {
+            'default': 'كودى كىرگۈزۈڭ SEDOL ئۈنۈملۈك'
+        },
+        siren: {
+            'default': 'نۇمۇرىنى كىرگۈزۈڭ SIREN ئۈنۈملۈك'
+        },
+        siret: {
+            'default': 'نۇمۇرىنى كىرگۈزۈڭ SIRET ئۈنۈملۈك'
+        },
+        step: {
+            'default': ' %s ھەر قەدەمدىكى ئۈنۈملۈك ھەسسىسى '
+        },
+        stringCase: {
+            'default': 'كىچىك يىزىلىشىدا كىرگۈزۈڭ',
+            upper: 'چوڭ يىزىلىشىدا كىرگۈزۈڭ'
+        },
+        stringLength: {
+            'default': 'ئۇزۇنلۇق چەكلىمىسىدىن ئاشۇرۇۋەتمەڭ',
+            less: ' دىن كۆپ بولسا بولمايدۇ %s',
+            more: ' بولسۇن %s ئەڭ ئاز بولغاندا',
+            between: 'گىچە بولسۇن %s دىن %s'
+        },
+        uri: {
+            'default': ' ئادىرىسى كىرگۈزۈڭ URL ئۈنۈملۈك'
+        },
+        uuid: {
+            'default': 'نۇمۇرىنى كىرگۈزۈڭ UUID ئۈنۈملۈك',
+            version: ' نۇمۇرىنى كىرگۈزۈڭ UUID نەشرىنىڭ %s'
+        },
+        vat: {
+            'default': 'نۇمۇرىنى كىرگۈزۈڭ VAT ئۈنۈملۈك',
+            countryNotSupported: '%s  قوللىمايدىغان دۆلەت ۋە رايون',
+            country: ' نۇمۇرىنى كىرگۈزۈڭ VAT نىڭ ئۈنۈملۈك %s',
+            countries: {
+                AT: 'Austria',
+                BE: 'Belgium',
+                BG: 'Bulgaria',
+                BR: 'Brazil',
+                CH: 'Switzerland',
+                CY: 'Cyprus',
+                CZ: 'Czech Republic',
+                DE: 'Germany',
+                DK: 'Denmark',
+                EE: 'Estonia',
+                ES: 'Spain',
+                FI: 'Finland',
+                FR: 'France',
+                GB: 'United Kingdom',
+                GR: 'Greece',
+                EL: 'Greece',
+                HU: 'Hungary',
+                HR: 'Croatia',
+                IE: 'Ireland',
+                IS: 'Iceland',
+                IT: 'Italy',
+                LT: 'Lithuania',
+                LU: 'Luxembourg',
+                LV: 'Latvia',
+                MT: 'Malta',
+                NL: 'Netherlands',
+                NO: 'Norway',
+                PL: 'Poland',
+                PT: 'Portugal',
+                RO: 'Romania',
+                RU: 'Russia',
+                RS: 'Serbia',
+                SE: 'Sweden',
+                SI: 'Slovenia',
+                SK: 'Slovakia',
+                VE: 'Venezuela',
+                ZA: 'South Africa'
+            }
+        },
+        vin: {
+            'default': 'نۇمۇرىنى كىرگۈزۈڭ VIN ئۈنۈملۈك'
+        },
+        zipCode: {
+            'default': 'ئۈنۈملۈك پوچتا نۇمۇرىنى كىرگۈزۈڭ',
+            countryNotSupported: '%s  قوللىمايدىغان دۆلەت ۋە رايون',
+            country: ' نىڭ ئۈنۈملۈك پوچتا نۇمۇرىنى كىرگۈزۈڭ %s',
+            countries: {
+                AT: 'Austria',
+                BR: 'Brazil',
+                CA: 'Canada',
+                CH: 'Switzerland',
+                CZ: 'Czech Republic',
+                DE: 'Germany',
+                DK: 'Denmark',
+                FR: 'France',
+                GB: 'United Kingdom',
+                IE: 'Ireland',
+                IT: 'Italy',
+                MA: 'Morocco',
+                NL: 'Netherlands',
+                PT: 'Portugal',
+                RO: 'Romania',
+                RU: 'Russia',
+                SE: 'Sweden',
+                SG: 'Singapore',
+                SK: 'Slovakia',
+                US: 'USA'
+            }
+        }
+    });
+}(window.jQuery));
